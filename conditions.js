a7e9bfad8c519c740e8b5c83fc4067f0281342f9
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-09-21 12:49:35 +0545

    conditions statements

---
+++ conditions.js
@@ -0,0 +1,41 @@
+const isadmin=false
+if(isadmin){
+    console.log("welcome to admin dashboard")
+}
+else{
+    console.log("You are not admin")
+}
+
+//mark 
+let percentage=67;
+if(percentage>=90&&percentage<=100){
+    console.log("Grade:A+")
+}
+else if(percentage>=80&&percentage<90){
+    console.log("Grade:A")
+}
+else if(percentage>=70&&percentage<80){
+    console.log("Grade:B+")
+}
+else if(percentage>=60&&percentage<70){
+    console.log("Grade:B")
+}
+else if(percentage>=50&&percentage<60){
+    console.log("Grade:C+")
+}
+else if(percentage>=40&&percentage<50){
+    console.log("Grade:A")
+}
+else if(percentage>=0&&percentage<40){
+    console.log("You arer fail")
+}
+
+let mark=80
+mark>=40?console.log("You are pass"):console.log("You are fail")
+
+//for loop
+
+
+
+
+
--- object.js
+++ object.js
@@ -9,3 +9,13 @@ console.log(board)
 console.log(typeof board)
 
 //array creations
+let arr=[1,4,6,8,3,4]
+console.log(arr[4])
+arr[2]=89
+console.log(arr)
+
+let fruits=["apple","bannana","orange","mango"];
+fruits[0]="kiwi"
+console.log(fruits[0])
+console.log(fruits.toString())
+
