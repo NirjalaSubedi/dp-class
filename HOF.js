8ffd753c7f13c6a03ea0e59e655b66f9f0a1592c
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-09-22 13:28:54 +0545

    Add mapping examples and console logs for array transformations in HOF.js

--- HOF.js
+++ HOF.js
@@ -13,3 +13,21 @@ num.forEach((data)=>{
     if(data%2==0)
         console.log("Even numbers in array:",data)
 })
+console.log("------------------------------")
+//Mapping
+
+const newarr=arr.map((num1)=>{
+        return num1+1
+})
+console.log(newarr)
+
+//Note:-> map le value return garxa vane foreach le value return gardaiina
+
+//Another example of mapping
+
+let MapArr=[1,2,3,4,5,6,7]
+let Mapped=MapArr.map((num)=>{
+    return num-1
+})
+console.log(MapArr)
+console.log(Mapped)
