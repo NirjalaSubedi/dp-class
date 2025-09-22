a6cadb2b46156862d385fbb72f9046d7fa5e4c24
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-09-22 12:32:09 +0545

    Refactor array.js to improve method logging and structure

--- array.js
+++ array.js
@@ -1,5 +1,13 @@
 //using array
 const arr=[1,5,8,9,3]
+
+//length properties
 console.log(arr.length)
 //paranthis xa vane thiyo method ho xaiina vane properties ho
+
+//at method
+console.log(arr.at(2))
+
+//join method
+console.log(arr.join("-"))
 
