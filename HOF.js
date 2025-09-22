d60ce8c3d2e072ca9c54134c7e58d345c61abd84
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-09-22 13:19:56 +0545

    Update array values and enhance logging in foreach function in HOF.js

--- HOF.js
+++ HOF.js
@@ -1,5 +1,15 @@
 //foreach function
-const arr=[5,9,6,8]
+const arr=[5,94,6,58,7,9,12,34,67,]
 arr.forEach((x)=>{
-    console.log(x+1)
+   // console.log(x+1)
+   if(x>10){
+    console.log(x)
+   }
+})
+
+//Another example
+const num=[3,6,8,22,34,12,1,5,7]
+num.forEach((data)=>{
+    if(data%2==0)
+        console.log("Even numbers in array:",data)
 })
