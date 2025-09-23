c33dcd8585b591f2a2556e06247d02ebc7ff19b2
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-09-23 12:40:28 +0545

    Add string filtering and mapping functionality in Filter.js

--- Filter.js
+++ Filter.js
@@ -4,3 +4,12 @@ const result=num.filter((numFilter)=>{
     return(numFilter>10)
 })
 console.log(result)
+
+//filtering in string
+
+const str=['a','e','i','o','u']
+const strResult=str.map((strFilter)=>{
+    return(strFilter.toUpperCase())
+})
+
+console.log(strResult)
