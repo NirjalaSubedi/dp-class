6a8784856db21dc41e3ff54a629603268f28e0ad
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-09-24 13:47:08 +0545

    Add button functionality and update HTML structure in button.html, index1.html, page.html, and script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+//const demo1=document.getElementById("demo").innerHTML="nirjala"
+// const container=document.getElementById("container")
+// document.write("helllo")
+
+// const newh1=document.createElement("h1")
+// const newh2=document.createElement("h2")
+// const newh3=document.createElement("h3")
+
+// newh1.textContent="hello how are you h1"
+// newh2.textContent="hello how are you h2"
+// newh3.textContent="hello how are you h3"
+
+// demo1.style.Color="blue"
+// //append 
+// //prepand
+// //appendchild
+// demo1.append("hellllllloooo")
+// const demo1 = document.getElementById("demo");
+// demo1.innerHTML = "nirjala";
+
+const demo=document.getElementById("demo")
+demo.innerHTML="nirjala"
+        
+demo.style.backgroundColor = "lightyellow"; 
+     
+
+
+
+
+
